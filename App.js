7f6a368460297f345fa2a3dd5af7fada9e58acb8
Shubham Patrick <ahthneeuhl@gmail.com>
2022-05-03 13:48:58 +0530

    Create TrackTask App | add basic styles | handle task from InputText | render tasks in tasks Container

--- App.js
+++ App.js
@@ -1,20 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [taskText, setTaskText] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  const taskInputHandler = (enteredText) => {
+    setTaskText(enteredText);
+  };
+
+  const addTaskHandler = () => {
+    setTasks((current) => [...current, taskText]);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          onChangeText={taskInputHandler}
+          style={styles.textInput}
+          placeholder="Your task"
+        />
+        <Button title="Add task" onPress={addTaskHandler} />
+      </View>
+      <View style={styles.tasksContainer}>
+        {tasks.map((task) => (
+          <Text key={task}>{task}</Text>
+        ))}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+    paddingHorizontal: 16,
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    width: "70%",
+    marginRight: 8,
+    padding: 8,
+  },
+  tasksContainer: {
+    flex: 5,
   },
 });
